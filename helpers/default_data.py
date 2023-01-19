4cc919c4abbfc7b00ec7bce0fe17bb26a38fdd1c
James <pawson1988@me.com>
2023-01-19 22:08:20 +0100

    added default data to helpers to clean up

---
+++ helpers/default_data.py
@@ -0,0 +1,40 @@
+from helpers.date_and_time import get_date, get_time
+
+user_accounts = [
+    {"username": "james", "password": "James"},
+    {"username": "claudia", "password": "Claudia"},
+    {"username": "david", "password": "David"}
+]
+
+default_words = [
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"},
+    {"timestamp": f'{get_time()} - {get_date()}', "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"}
+]
--- words_app/models.py
+++ words_app/models.py
@@ -1,6 +1,6 @@
 from words_app import app
 from words_app import db
-from helpers.date_and_time import get_date
+from helpers.default_data import user_accounts, default_words
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
@@ -29,11 +29,7 @@ with app.app_context():
     # db.drop_all()
     db.create_all()
 
-user_accounts = [
-    {"username": "james", "password": "James"},
-    {"username": "claudia", "password": "Claudia"},
-    {"username": "david", "password": "David"}
-]
+
 
 # add users above to database
 with app.app_context():
@@ -44,15 +40,7 @@ with app.app_context():
             db.session.commit()
 
 
-
-# template word to show on show_words page 
-default_words = [
-    {"timestamp": get_date(), "word": "new", "user_id": 1, "part_of_speech": "adjective", "translation": "nuevo", "sentence": "This is an example of a sentence using this word"},
-    {"timestamp": get_date(), "word": "old", "user_id": 2, "part_of_speech": "adjective", "translation": "antiguo", "sentence": "This is an example of a sentence using this word"},
-    {"timestamp": get_date(), "word": "book", "user_id": 3, "part_of_speech": "noun", "translation": "libro", "sentence": "This is an example of a sentence using this word"}
-]
-
-# Add default word to database when app is run
+# Add default words to database when app is run
 with app.app_context():
     if not Word.query.all(): # if there isn't a word in the dictionary add the word(s) below
         for word in default_words:
