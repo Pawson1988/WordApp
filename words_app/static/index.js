8e196f36330c5df163dd9d6246118347774df7b2
James <pawson1988@me.com>
2023-01-21 16:48:24 +0100

    Added to word exercise in index.js

--- words_app/static/index.js
+++ words_app/static/index.js
@@ -16,8 +16,10 @@ if(window.location.pathname === "/get_translation"){
 const cards = document.querySelectorAll(".card-multiple")
 const chosen_words = document.querySelector(".chosen_words")
 const clear_words_button = document.querySelector(".clear_words_btn")
+const sentence = document.querySelector(".sentence")
+const check_btn = document.querySelector(".check_sentence")
 
-
+let selectedWords = []
 
 
 for(let card of cards){
@@ -26,20 +28,38 @@ for(let card of cards){
         if(card.style.top != "10rem"){
            console.log(card)
             chosen_words.textContent += `${card.childNodes[1].textContent} `
+            selectedWords.push(card.childNodes[1].textContent)
+            console.log(selectedWords)
             card.style.position = "relative"
             card.style.top = "10rem"
             card.style.zIndex++  
             chosen_words.style.marginTop = "10rem"
         } else {
             resetCardPositions(card)
+            selectedWords = []
         }
     })
 }
 
 clear_words_button.addEventListener("click", () => {
-    chosen_words.textContent = "No Words"
+    chosen_words.textContent = ""
     for(let card of cards){
         resetCardPositions(card)
+        selectedWords = []
+        sentence.value = ""
     }
 })
+
+check_btn.addEventListener("click", () => {
+    if(sentence.value){
+        let isEvery = selectedWords.every(item => sentence.value.toLowerCase().includes(item.toLowerCase()))
+        if(isEvery){
+            sentence.style.backgroundColor = "green"
+        } else {
+            sentence.style.backgroundColor = "red"
+        }
+    }
+    
+})
+
 
